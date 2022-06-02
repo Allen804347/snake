592cb750e953b443af677be7d5b6ca1b101dd6a1
Allen <Allen804337@gmail.com>
2022-06-03 00:51:25 +0800

    feat: add border

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 // snake
 // time machine
 // food
+const borderSize = 1
 const grid = 10
 </script>
 
@@ -10,24 +11,25 @@ const grid = 10
     :style="{
       width: `${15 * grid}px`,
       height: `${15 * grid}px`,
+      border: `${borderSize}px solid black`,
     }"
   >
     <div
-      style="background-color: black;position:absolute;"
+      style="background-color: black; position: absolute;"
       :style="{
         width: `${1 * grid}px`,
         height: `${1 * grid}px`,
-        top: `${7 * grid}px`,
-        left: `${3 * grid}px`,
+        top: `${borderSize + (7 * grid)}px`,
+        left: `${borderSize + (3 * grid)}px`,
       }"
     />
     <div
-      style="background-color: red;position:absolute;"
+      style="background-color: red; position: absolute;"
       :style="{
         width: `${1 * grid}px`,
         height: `${1 * grid}px`,
-        top: `${3 * grid}px`,
-        left: `${3 * grid}px`,
+        top: `${borderSize + (3 * grid)}px`,
+        left: `${borderSize + (3 * grid)}px`,
       }"
     />
   </div>
