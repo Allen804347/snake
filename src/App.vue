a20c91b3c70ff70a38dfef3b5da5ddf343862a58
Allen <Allen804337@gmail.com>
2022-06-30 23:59:21 +0800

    feat: add fruit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <script setup lang="ts">
+import { onMounted } from '@vue/runtime-core'
 import Snake from './components/Snake.vue'
 // snake
-const MSperTick = 1000
+const MSperTick = 500
 // food
 const borderSize = 1
-const mapSize = 15
+const mapSize = 25
 const grid = 10
 
 // time engine
 let task: number
+// snake
 const snake = $ref(null)
+// fruit
+let fruitX = $ref(0)
+let fruitY = $ref(0)
+
+// random show
+const renewFruit = () => {
+  const newX = Math.ceil(Math.random() * mapSize)
+  const newY = Math.ceil(Math.random() * mapSize)
+  // fruit should not born on snake
+  if (!snake.checkCollision(newX, newY)) {
+    fruitX = newX
+    fruitY = newY
+    return
+  }
+  // re renew
+  renewFruit()
+}
 
 const start = (): void => {
   if (task !== null)
@@ -19,22 +38,29 @@ const start = (): void => {
     // snake move
     snake.move()
     // check fruit
-    // growth snake if need
+    if (snake.checkCollision(fruitX, fruitY)) {
+      // growth snake if need
+      // renew fruit if need
+      renewFruit()
+    }
     // check snake is live or death
     const { x, y } = snake.getHead()
-    if (snake.checkCollision() || x <= 0 || y <= 0 || x > mapSize || y > mapSize)
+    if (snake.checkSelfCollision() || x <= 0 || y <= 0 || x > mapSize || y > mapSize)
       snake.die()
-    // renew fruit if need
   }, MSperTick)
 }
 start()
+
+// init
+onMounted(renewFruit)
+// can be eat
 </script>
 
 <template>
   <div
     :style="{
-      width: `${15 * grid}px`,
-      height: `${15 * grid}px`,
+      width: `${mapSize * grid}px`,
+      height: `${mapSize * grid}px`,
       border: `${borderSize}px solid black`,
     }"
   >
@@ -44,8 +70,8 @@ start()
       :style="{
         width: `${1 * grid}px`,
         height: `${1 * grid}px`,
-        top: `${borderSize + (3 * grid)}px`,
-        left: `${borderSize + (3 * grid)}px`,
+        top: `${borderSize + (fruitY * grid)}px`,
+        left: `${borderSize + (fruitX * grid)}px`,
       }"
     />
   </div>
